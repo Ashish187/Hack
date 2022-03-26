96ffd8d00ddcb81cd74b3ab654f0c3f010c79f99
Ashish187 <ashishanand16086@gmail.com>
2022-03-26 08:45:22 +0530

    fourth commit

---
+++ front/src/components/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from 'react-bootstrap'
+import { useNavigate } from 'react-router'
+
+export default function Home() {
+    const history = useNavigate()
+    async function handleDash(){
+        history("/dashboard")
+    }
+  return (
+    <>
+    
+      <div className="d-flex">
+              <Button className="w-100 mt-4 mx-4 d-flex align-items-center justify-content-center" variant="success" onClick={handleDash}>Upload Project</Button>
+
+              <Button className="w-100 mt-4 d-flex align-items-center justify-content-center" variant="success">Search Project</Button>
+          </div>
+          </>
+  )
+}
